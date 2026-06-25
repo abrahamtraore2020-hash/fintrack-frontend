ee4ca4057ecb897680b000427f8afb053ac5465c
Abraham Traore <abrahamtraore2020@gmail.com>
2026-06-25 18:46:47 +0000

    feat: illustrations africaines sur toutes les pages via AppLayout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from './Navbar'
 import { Sidebar } from './Sidebar'
 import { BottomNav } from './BottomNav'
+import { BackgroundDecor } from '@/components/ui/AfricanIllustrations'
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col relative overflow-hidden">
+      <BackgroundDecor />
       <Navbar />
-      <div className="flex flex-1">
+      <div className="flex flex-1 relative z-10">
         <Sidebar />
         <main className="flex-1 p-4 md:p-5 overflow-y-auto animate-fade-in pb-24 md:pb-5">
           {children}
